63945eddc369774fa41e344031665a91cafa464d
GermanSar <78889433+GermanSar@users.noreply.github.com>
2021-03-09 14:27:36 -0500

    Add files via upload

---
+++ EJERCICIO 10 DUAL CORE/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("setup() running on core ");
+  Serial.println(xPortGetCoreID());
+}
+
+void loop() {
+  Serial.print("loop() running on core ");
+  Serial.println(xPortGetCoreID());
+}
